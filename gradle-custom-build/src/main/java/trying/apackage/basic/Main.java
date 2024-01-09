5c9bd1ef8046ef0b01b97e4ff1f900f264d0911b
pvi-axelor <p.vishal@axelor.com>
2024-01-09 10:59:38 +0530

    Added a custom built gradle java project

---
+++ gradle-custom-build/src/main/java/trying/apackage/basic/Main.java
@@ -0,0 +1,12 @@
+package trying.apackage.basic;
+
+public class Main {
+
+    public static void callThisMethod() {
+        System.out.println("WHO DARES SUMMON ME ???");
+    }
+
+    public static void main(String[] args) {
+        Main.callThisMethod();
+    }
+}
